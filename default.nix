6635946946a41d748d26ead00641036e93a8e23a
Oleksandr <olex.ceo@gmail.com>
2025-12-25 20:03:53 +0200

    default.nix

---
+++ default.nix
@@ -0,0 +1,7 @@
+# default.nix
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.writers.writePython3Bin "skyswitcher" {
+  libraries = [ pkgs.python3Packages.evdev ];
+  flakeIgnore = [ "E501" ];
+} (builtins.readFile ./main.py)
---
+++ flake.nix
@@ -0,0 +1,26 @@
+# flake.nix
+{
+  description = "SkySwitcher";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = import ./default.nix { inherit pkgs; };
+
+      # Щоб працювало `nix develop` (середовище розробки)
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (python3.withPackages (ps: [ ps.evdev ]))
+          wl-clipboard
+          evtest
+        ];
+      };
+    };
+}
