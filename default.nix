e871ccf76eb9086c59719389712fa4663145855e
Oleksandr <olex.ceo@gmail.com>
2025-12-27 17:02:09 +0200

    fix project name

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.writers.writePython3Bin "skyswitcher" {
+pkgs.writers.writePython3Bin "magshift" {
   libraries = [ pkgs.python3Packages.evdev ];
   flakeIgnore = [ "E501" ];
 } (builtins.readFile ./main.py)
