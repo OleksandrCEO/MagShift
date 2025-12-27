b051c104dc4059246254ef06f6361d602f2c8653
Oleksandr <olex.ceo@gmail.com>
2025-12-27 17:34:07 +0200

    fix user access

--- module.nix
+++ module.nix
@@ -11,22 +11,22 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    # 1. Install the package to system path so it can be found by KDE Autostart
+    # 1. Install the package to system path
     environment.systemPackages = [ pkgs.magshift ];
 
-    # 2. Enable uinput kernel module to ensure /dev/uinput device is created
+    # 2. Enable uinput kernel module
     hardware.uinput.enable = true;
 
     # 3. Configure dynamic permissions via Udev rules.
-    # Instead of adding the user to static 'input' or 'uinput' groups (which is a security risk),
-    # we use the 'uaccess' tag. This grants read/write access to input devices
-    # ONLY to the user currently logged into the active graphical session (via systemd-logind).
+    # We use 'uaccess' and 'seat' tags to grant R/W access
+    # ONLY to the user currently logged into the active physical session.
     services.udev.extraRules = ''
       # Grant access to create virtual input devices (uinput) to the active user
-      KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
+      KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", TAG+="seat", ENV{ID_SEAT}="seat0", OPTIONS+="static_node=uinput"
 
       # Grant access to read physical keyboard events to the active user
-      SUBSYSTEM=="input", KERNEL=="event*", ENV{ID_INPUT_KEYBOARD}=="1", TAG+="uaccess"
+      # IMPORT{builtin}="input_id" is often handled by default rules, but we ensure it's there
+      SUBSYSTEM=="input", KERNEL=="event*", ENV{ID_INPUT_KEYBOARD}=="1", TAG+="uaccess", TAG+="seat", ENV{ID_SEAT}="seat0"
     '';
   };
 }
